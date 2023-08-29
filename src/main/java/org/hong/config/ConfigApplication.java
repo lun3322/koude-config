eb819bf00fc5b0a6c7070be5619c60822b665f0b
hong <lun3322@126.com>
2023-08-29 11:28:39 +0800

    更新 项目配置完成

--- src/main/java/org/hong/config/ConfigApplication.java
+++ src/main/java/org/hong/config/ConfigApplication.java
@@ -2,12 +2,17 @@ package org.hong.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * @author hong
+ */
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConfigApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
 
 }
